4c290d6a50f94965d14e9336b3d91b0e88733a38
vranjesluka <vranjes.luka@gmail.com>
2016-09-09 11:35:22 +0200

    Initial project set up

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Luka Vranješ on 09/09/16.
+//  Copyright © 2016 Luka Vranješ. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    override func drawRect(rect: CGRect)
+    {
+//        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+//        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+//        
+//        let 
+    }
+
+}
---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Luka Vranješ on 08/09/16.
+//  Copyright © 2016 Luka Vranješ. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+}
+
